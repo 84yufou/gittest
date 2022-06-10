4e7acfa3260225af59308c070a9325a869a2ee10
yufou <yufou@yufou.com>
2022-06-10 17:58:22 +0800

    first commit

---
+++ src/main/java/com/yufou/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.yufou.git;
+
+/**
+ * <h5>描述:</h5>
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+        }
